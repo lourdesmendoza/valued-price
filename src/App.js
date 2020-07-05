f8854e236bea9a4cd907b5f18417e60b845cb125
Lourdes Mendoza <lmendoza@teco.com.ar>
2020-07-05 14:15:23 -0300

    Added styled components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+
 import Header from './components/Header';
 
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+
+const ContentHeader = styled.header `
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
+
+const TextHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Ubuntu',serif;
+    text-align: center;
+`
 
 const Header = ({ title }) => {
     return ( 
-        <header>
-            <h1>{title}</h1>
-        </header>
+        <ContentHeader>
+            <TextHeader>{title}</TextHeader>
+        </ContentHeader>
      );
 }
  
