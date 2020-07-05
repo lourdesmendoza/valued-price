7b8edf2a1a5cc7054e8cfb2bf0fdbaefe91e7211
Lourdes Mendoza <lmendoza@teco.com.ar>
2020-07-05 14:36:14 -0300

    Added component form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-
+import styled from '@emotion/styled'; 
 
 import Header from './components/Header';
+import Form from './components/Form';
+
+const Content = styled.div`
+	max-width: 600px;
+	margin: 0 auto;
+`;
+
+const ContentForm = styled.div`
+	background-color: #ffffff;
+	padding: 3rem;
+`;
+
 
 function App() {
 	return (
-		<Header
-			title="Insurance Quote"
-			/>
+		<Content>
+			<Header
+				title="Insurance Quote"
+				/>
+				<ContentForm>
+					<Form />
+				</ContentForm>
+		</Content>
 	);
 }
 
